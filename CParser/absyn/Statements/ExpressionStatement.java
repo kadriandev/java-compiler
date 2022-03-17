4408e3de932cfee08dad23568f902f3cbe3447da
Frank Maciel <fmaciel@fred.socs.uoguelph.ca>
2022-03-17 12:53:43 -0400

    Added NodeType.java, as well as SemanticAnalyzer.java

--- CParser/absyn/Statements/ExpressionStatement.java
+++ CParser/absyn/Statements/ExpressionStatement.java
@@ -1,4 +1,8 @@
 package absyn.Statements;
+import absyn.Absyn;
+import absyn.AbsynVisitor;
+import absyn.Declaration.Declaration;
 
 public abstract class ExpressionStatement extends Statement {
+    public Declaration dtype;
 }
