83900feaa3d2b70c934cc37ad6f87e2e34eacb4c
Kyle Monteiro <kyle.monteiro@gmail.com>
2022-03-20 14:57:52 -0400

    Added NodeType.java, as well as SemanticAnalyzer.java (#1)
    Co-authored-by: Frank Maciel <fmaciel@fred.socs.uoguelph.ca>

---
+++ CParser/absyn/NodeType.java
@@ -0,0 +1,16 @@
+package absyn;
+
+public class NodeType{
+    String name;
+    Dec def;
+    int level;
+
+    NodeType ({
+        required this.name,
+        required this.def,
+        required this.level,
+    });
+
+
+}
+
